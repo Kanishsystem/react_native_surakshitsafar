54bec9dd5e7cc5f823c385b558d302095ff3904e
Yuvaranidass <159615453+Yuvaranidass@users.noreply.github.com>
2024-03-18 13:10:29 +0530

    update

--- src/Pages/PermitHolder/Dashboard.jsx
+++ src/Pages/PermitHolder/Dashboard.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+import { Card } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import DashboardStyles from "../../Styles/DashboardStyles";
 
 const Dashboard = () => {
   return (
-    <SafeAreaView>
-        <View>
-            <Text>SU</Text>
+    <SafeAreaView style={DashboardStyles.container}>
+      <View style={DashboardStyles.headerstyle}>
+        <View style={DashboardStyles.HeadTexrStyle}>
+          <Text style={DashboardStyles.Textstyle}>SURAKSHIT SAFAR</Text>
+          <MaterialCommunityIcons
+            //   onPress={() => handleBack()}
+            name="menu"
+            color={"white"}
+            size={25}
+          />
         </View>
-
+      </View>
+      <View style={DashboardStyles.bodyview}>
+        <Card style={DashboardStyles.cardView}>
+<Card.Content>
+    <View>
+    
+    </View>
+   
+</Card.Content>
+        </Card>
+      </View>
     </SafeAreaView>
+  );
+};
 
-  )
-}
-
-export default Dashboard
+export default Dashboard;
---
+++ src/Styles/DashboardStyles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const DashboardStyles = StyleSheet.create({
+  headerstyle:{
+   display:"flex",
+   
+   
+    backgroundColor:"#0a4b43",
+    width:410,
+    height:100
+  },
+  container:{
+    flex:1,
+    padding:15,
+  },
+  Textstyle:{
+    color:"white",
+    fontSize:22,
+  },
+  HeadTexrStyle:{
+    marginTop:35,
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    padding:15,
+  },
+  cardView:{
+height:70,
+marginTop:10,
+  },
+  bodyview:{
+    padding:10
+  },
+    
+
+});
+
+export default DashboardStyles;
