a61962600cfad0abaae893debcb80ae09dd53406
gangavishnu12 <chinabhai80@gmail.com>
2024-03-18 13:09:39 +0530

    updated

---
+++ src/Styles/LoginStyles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const LoginStyles = StyleSheet.create({
+    safeView: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    //   marginTop:25,
+      backgroundColor: "#0c4b43",
+      width:"100%"
+    },
+    viewBlock:{
+      padding:5,
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+    }
+  });
+  
+  export default LoginStyles;
+  
---
+++ src/Styles/TextStyles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const TextStyles = StyleSheet.create({
+    Title: {
+      fontWeight: "bold",
+    //   textAlign:"center",
+    display:"flex",
+      color:"white",
+      fontSize:24,
+      marginTop:7
+    },
+  });
+  
+  export default TextStyles;
+  
