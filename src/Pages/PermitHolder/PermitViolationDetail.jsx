a06e3666b8aeebd6365d52210c44632044343a9b
Yuvaranidass <159615453+Yuvaranidass@users.noreply.github.com>
2024-03-19 12:26:25 +0530

    update

---
+++ src/Pages/PermitHolder/PermitViolationDetail.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { SafeAreaView, View, Text, Image, TextInput } from "react-native";
+import { Card } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import DashboardStyles from "../../Styles/DashboardStyles";
+import { useNavigation } from "@react-navigation/native";
+import {
+  SmartSoftButton,
+  SmartSoftForm,
+  SmartSoftColumns,
+  SmartSoftColumn,
+} from "soft_digi_native";
+import {} from "../../Services/ImageServices";
+import SmartInput from "soft_digi_native/dist/forms/SmartInput";
+
+const PermitViolationDetail = () => {
+    const data = [
+        { Name: "Violation Location", Holder: "150, sector20, Gurugram,Haryana," },
+        { Name: "Permit Type", Holder: "GoodsCarrierPermit" },
+        { Name: "Violation State", Holder: "ASSAM" },
+        { Name: "Violation Details", Holder: "TemporaryPermit" },
+        { Name: "Violation Type", Holder: "TemporaryPermit" },
+        { Name: "Valid State", Holder: "HARYANA" },
+        { Name: "Violation Time", Holder: "2023-10-12 16:07:23" },
+        {title:"Vehicle Current Detais"},
+
+        { Name: "Vehicle No.", Holder: "HR26AW7865" },
+
+        { Name: "Valid State", Holder: "OFF" },
+
+        { Name: "Valid State", Holder: "0" },
+
+        { Name: "Valid State", Holder: "sector 44,Gurugram,Haryana" },
+
+        { Name: "Latitude", Holder: "28.45225" },
+
+        { Name: "Longitude", Holder: "77.0696" },
+
+        
+      ];
+    
+  return (
+    <SafeAreaView style={DashboardStyles.container}>
+      <View style={DashboardStyles.headerstyle}>
+        <View style={DashboardStyles.HeadTextStyle}>
+          <Text style={DashboardStyles.Textstyle}>Permit Violation Detail</Text>
+        </View>
+      </View>
+      <View style={DashboardStyles.VolatileBodyView}>
+
+     
+      <Card style={DashboardStyles.volatileCard}>
+        <Card.Content>
+            <Text style={DashboardStyles.BusViewText}>Permit Violation Details</Text>
+            {data.map((item, index) => (
+                <View>
+            <Card style={DashboardStyles.volatilecardview}>
+                <Card.Content>
+                    <View style={DashboardStyles.VolatileView}>
+                    
+                        <Text>{item.Name}</Text>
+                        <Text>{item.Holder}</Text>
+                    </View>
+                </Card.Content>
+            </Card>
+            <Text style={DashboardStyles.BusViewText}>{item.title}</Text>
+            </View>
+            ))}
+        </Card.Content>
+      </Card>
+
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default PermitViolationDetail;
--- src/Styles/DashboardStyles.ts
+++ src/Styles/DashboardStyles.ts
@@ -1,122 +1,149 @@
 import { StyleSheet } from "react-native";
 
 const DashboardStyles = StyleSheet.create({
-  headerstyle:{
-   display:"flex",
-   
-   
-    backgroundColor:"#0a4b43",
-    width:410,
-    height:100
-  },
-  container:{
-    flex:1,
-    padding:15,
-  },
-  Textstyle:{
-    color:"white",
-    fontSize:22,
-  },
-  HeadTexrStyle:{
-    marginTop:35,
-    display:"flex",
-    flexDirection:"row",
-    justifyContent:"space-between",
-    padding:15,
-  },
-  cardView:{
-height:"auto",
-marginTop:10,
-  },
-  bodyview:{
-    padding:10
-  },
-  button:{
-    backgroundColor:"#0a4b43",
-    height:60,
-    alignItems:"center",
-    width:170
-  },
-  buttonview:{
-display:"flex",
-flexDirection:"row",
-justifyContent:"space-around",
+  headerstyle: {
+    display: "flex",
 
+    backgroundColor: "#0a4b43",
+    width: 410,
+    height: 100,
   },
-  profilecardView:{
-flexDirection:"row",
-justifyContent:"space-between",
+  container: {
+    flex: 1,
+    padding: 15,
   },
-  profileText:{
-    fontSize:18,
-    color:"#0a4b43",
-
+  Textstyle: {
+    color: "white",
+    fontSize: 22,
   },
-  profileTextview:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-
+  HeadTextStyle: {
+    marginTop: 35,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
   },
-  featureTextStyle:{
-    color:"#0a4b43",
-    fontWeight:"bold",
-    marginTop:10,
-    fontSize:17
+  cardView: {
+    height: "auto",
+    marginTop: 10,
+  },
+  bodyview: {
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "#0a4b43",
+    height: 60,
+    alignItems: "center",
+    width: 170,
+  },
+  buttonview: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  profilecardView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  profileText: {
+    fontSize: 18,
+    color: "#0a4b43",
+  },
+  profileTextview: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  featureTextStyle: {
+    color: "#0a4b43",
+    fontWeight: "bold",
+    marginTop: 10,
+    fontSize: 17,
   },
 
-  featureCard:{
-    height:160,
-    width:"95%",
-    marginTop:10,
-    borderBottomWidth:6,
-    borderColor:"#0a4b43",
+  featureCard: {
+    height: 160,
+    width: "95%",
+    marginTop: 10,
+    borderBottomWidth: 6,
+    borderColor: "#0a4b43",
   },
-  MonitoringImage:{
-    height:70,
-    width:70,
-    justifyContent:"center",
-    alignItems:"center",
+  MonitoringImage: {
+    height: 70,
+    width: 70,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  featureCardView: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  MonitoringText: {
+    marginTop: 10,
+  },
+  featureview: {
+    display: "flex",
+    flexDirection: "row",
+  },
+
+  //LiveMontoring style codes
+  LiveMonitoringImage: {
+    height: 300,
+    width: 400,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 200,
+  },
+
+  searchContainer: {},
+
+  searchInput: {
+    height: 60,
+    width: "100%",
+    borderWidth: 3,
+    borderColor: "#ced3d6",
+  },
+
+  VechicleText: {
+    marginTop: 15,
+    fontSize: 20,
+  },
+  VechicleCard:{
+    backgroundColor:"#85a5a2",
+    height:"auto",
     marginTop:10
   },
-  featureCardView:{
+  BusView:{
     display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
+    flexDirection:"row",
   },
-  MonitoringText:{
-marginTop:10
+  BusViewText:{
+    fontSize:20,
+    
   },
-  featureview:{
+  volatileCard:{
+    marginTop:10,
+    backgroundColor:"white",
+    
+  },
+  VolatileBodyView:{
+    backgroundColor:"#e3e5e6"
+
+  },
+  VolatileView:{
     display:"flex",
-flexDirection:"row",
-
+    flexDirection:"row",
+    justifyContent:"space-between",
+    
   },
+  volatilecardview:{
+    height:50,
+    borderWidth:2,
+    borderColor:"#bcc1c2",
+    marginTop:7
 
-
-//LiveMontoring style codes
-  LiveMonitoringImage:{
-    height:300,
-    width:400,
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    marginTop:200
-  },
-
-
-
-
-
-  searchContainer:{
-
-  },
-
-  searchInput:{
-
-    height:60,
-    width:"100%",
-    borderWidth:3,
-    borderColor:"#ced3d6"
   },
 });
 
